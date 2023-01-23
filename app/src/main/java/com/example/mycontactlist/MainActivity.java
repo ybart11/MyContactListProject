8de31d604a4f950f3a0a8fa261fa3268176a1169
yovany_23 <yovany1061@gmail.com>
2023-01-23 15:20:24 -0500

    Added initToggleButton and tested successfully!

--- app/src/main/java/com/example/mycontactlist/MainActivity.java
+++ app/src/main/java/com/example/mycontactlist/MainActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         initListButton();
         initMapButton();
         initSettingsButton();
+        setForEditing(false);
+        initToggleButton();
+
     }
 
     // Associate the ImageButton named imageButtonList on the activity_main layout
@@ -67,4 +73,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    // Initialize button to respond to the user
+    private void initToggleButton() {
+
+        final ToggleButton editToggle = (ToggleButton) findViewById(R.id.toggleButtonEdit);
+
+        editToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setForEditing(editToggle.isChecked());
+            }
+        });
+    }
+
+    // Button that enables all the data entry widgets
+    private void setForEditing (boolean enabled) {
+
+        EditText editName = findViewById(R.id.editName);
+        EditText editAddress = findViewById(R.id.editAddress);
+        EditText editCity = findViewById(R.id.editCity);
+        EditText editState = findViewById(R.id.editState);
+        EditText editZipCode = findViewById(R.id.editZipcode);
+        EditText editPhone = findViewById(R.id.editHome);
+        EditText editCell = findViewById(R.id.editCell);
+        EditText editEmail = findViewById(R.id.editEMail);
+
+        Button buttonChange = findViewById(R.id.btnBirthday);
+        Button buttonSave = findViewById(R.id.buttonSave);
+
+
+        editName.setEnabled(enabled);
+        editAddress.setEnabled(enabled);
+        editCity.setEnabled(enabled);
+        editState.setEnabled(enabled);
+        editZipCode.setEnabled(enabled);
+        editPhone.setEnabled(enabled);
+        editCell.setEnabled(enabled);
+        editEmail.setEnabled(enabled);
+        buttonChange.setEnabled(enabled);
+        buttonSave.setEnabled(enabled);
+
+        if (enabled) {
+            editName.requestFocus();
+        }
+    }
+
+
 }
