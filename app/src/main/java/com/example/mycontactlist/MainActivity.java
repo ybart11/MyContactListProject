66526d021cdc160a92a01bf4b5bd592476830ed6
yovany_23 <yovany1061@gmail.com>
2023-01-23 14:27:28 -0500

    created method associated with imageButtonList

--- app/src/main/java/com/example/mycontactlist/MainActivity.java
+++ app/src/main/java/com/example/mycontactlist/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.mycontactlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListButton();
+    }
+
+    // Associate the ImageButton named imageButtonList on the activity_main layout
+    // with the code that is executed when it is pressed
+    private void initListButton() {
+
+        ImageButton ibList = findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ContactListActivity.class);
+
+                // Alert the operating system to not make multiple copies of the same activity
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
