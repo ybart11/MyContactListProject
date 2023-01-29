fd1d49f973f827a0d164a93fe2548cb222283a54
yovany_23 <yovany1061@gmail.com>
2023-01-28 20:34:28 -0500

    Chapter 5 Listing 5.1 & 5.2 done and tested successfully.

--- app/src/main/java/com/example/mycontactlist/ContactSettingsActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactSettingsActivity.java
@@ -2,10 +2,13 @@ package com.example.mycontactlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class ContactSettingsActivity extends AppCompatActivity {
 
@@ -15,10 +18,108 @@ public class ContactSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contact_settings);
         initListButton();
         initMapButton();
+        initSettingsButton();
+        initSettings();
+        initSortByClick();
+        initSortOrderClick();
     }
 
-    // Associate the ImageButton named imageButtonList on the activity_main layout
-    // with the code that is executed when it is pressed
+    /*
+
+    The initSettings method gets the values stored in SharedPreferences to set the RadioButtons
+        to the value that the user checks
+     */
+    private void initSettings() {
+        String sortBy = getSharedPreferences("MyContactListPreferences",
+                Context.MODE_PRIVATE).getString("sortfield", "contactname");
+        String sortOrder = getSharedPreferences("MyContactListPreferences",
+                Context.MODE_PRIVATE).getString("sortorder", "ASC");
+
+        RadioButton rbName = findViewById(R.id.radioName);
+        RadioButton rbCity = findViewById(R.id.radioCity);
+        RadioButton rbBirthDay = findViewById(R.id.radioBirthday);
+
+        // The value retrieved for the preferred sort field is evaluated to determine
+        // which RadioButton should be set as checked
+        if (sortBy.equalsIgnoreCase("contactname")) {
+            rbName.setChecked(true);
+        }
+        else if (sortBy.equalsIgnoreCase("city")) {
+            rbCity.setChecked(true);
+        }
+        else {
+            rbBirthDay.setChecked(true);
+        }
+
+        RadioButton rbAscending = findViewById(R.id.radioAscending);
+        RadioButton rbDescending = findViewById(R.id.radioDescending);
+
+        // Set the order preferred by the user
+        if(sortOrder.equalsIgnoreCase("ASC")) {
+            rbAscending.setChecked(true);
+        }
+        else {
+            rbDescending.setChecked(true);
+        }
+    }
+
+
+    /*
+       Method that stores the selected user preference for
+        each option.
+       When the user presses a RadioButton in one of the groups, the method determines
+        which RadioButton was pressed and then the value associated with that RadioButton
+        is saved in SharedPreferences.
+     */
+    // For RadioGroup "Sort Contacts By"
+    private void initSortByClick() {
+        RadioGroup rgSortBy = findViewById(R.id.radioGroupSortBy);
+        rgSortBy.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton rbName = findViewById(R.id.radioName);
+                RadioButton rbCity = findViewById(R.id.radioCity);
+
+                if (rbName.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences",
+                            Context.MODE_PRIVATE).edit()
+                            .putString("sortfield", "contactname").apply();
+                }
+                else if (rbCity.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences",
+                            Context.MODE_PRIVATE).edit()
+                            .putString("sortfield", "city").apply();
+                }
+                else {
+                    getSharedPreferences("MyContactListPreferences",
+                            Context.MODE_PRIVATE).edit()
+                            .putString("sortfield", "birthday").apply();
+                }
+            }
+        });
+    }
+
+    // For RadioGroup "Sort Order"
+    private void initSortOrderClick() {
+        RadioGroup rgSortOrder = findViewById(R.id.radioGroupSortOrder);
+        rgSortOrder.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton rbAscending = findViewById(R.id.radioAscending);
+
+                if(rbAscending.isChecked()) {
+                    getSharedPreferences("MyContactListPreferences",
+                            Context.MODE_PRIVATE).edit()
+                            .putString("sortorder", "ASC").apply(); // s: key, s1: value
+                }
+                else {
+                    getSharedPreferences("MyContactListPreferences",
+                            Context.MODE_PRIVATE).edit()
+                            .putString("sortorder", "DESC").apply();
+                }
+            }
+        });
+    }
     private void initListButton() {
 
         ImageButton ibList = findViewById(R.id.imageButtonList);
@@ -49,6 +150,12 @@ public class ContactSettingsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void initSettingsButton() {
+
+        ImageButton ibList = findViewById(R.id.imageButtonSettings);
+        ibList.setEnabled(false);
     }
 
 }
