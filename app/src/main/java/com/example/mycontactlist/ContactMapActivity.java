fd1fd04447fad04c601907c93bd32eaf841802e6
yovany_23 <yovany1061@gmail.com>
2023-02-10 12:24:53 -0500

    Listing 7.1 completed

--- app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
+++ app/src/main/java/com/example/mycontactlist/ContactMapActivity.java
@@ -3,9 +3,16 @@ package com.example.mycontactlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class ContactMapActivity extends AppCompatActivity {
 
@@ -15,6 +22,46 @@ public class ContactMapActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contact_map);
         initListButton();
         initSettingsButton();
+        initGetLocationButton();
+    }
+
+    private void initGetLocationButton () {
+        Button locationButton = (Button) findViewById(R.id.buttonGetLocation);
+        locationButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                EditText editAddress  = (EditText) findViewById(R.id.editTextMapAddress);
+                EditText editCity = (EditText) findViewById(R.id.editTextMapCity);
+                EditText editState = (EditText) findViewById(R.id.editTextMapState);
+                EditText editZipCode = (EditText) findViewById(R.id.editTextMapZipcode);
+
+                // Proper format for Geocoder
+                String address = editAddress.getText().toString() + ", " +
+                                    editCity.getText().toString() + ", " +
+                                    editState.getText().toString() + ", " +
+                                    editZipCode.getText().toString();
+
+                List <Address> addresses = null;
+                Geocoder geo = new Geocoder(ContactMapActivity.this);
+
+                try {
+                    addresses = geo.getFromLocationName(address, 1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                /* The latitude and longitude of the first address in the returned list are
+                    displayed in the appropriate TextView widgets*/
+                TextView txtLatitude = (TextView) findViewById(R.id.textLatitude);
+                TextView txtLongitude = (TextView) findViewById(R.id.textLongitude);
+
+                txtLatitude.setText(String.valueOf(addresses.get(0).getLatitude()));
+                txtLongitude.setText(String.valueOf(addresses.get(0).getLongitude()));
+
+            }
+        });
+
     }
 
     // Associate the ImageButton named imageButtonList on the activity_main layout
@@ -51,4 +98,12 @@ public class ContactMapActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void initMapButton() {
+
+        ImageButton ibList = findViewById(R.id.imageButtonMap);
+        ibList.setEnabled(false);
+    }
+
+
 }
