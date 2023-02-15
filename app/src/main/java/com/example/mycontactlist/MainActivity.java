8c53d0db31db8ad41fa2013cbe698758776fa2a1
yovany_23 <yovany1061@gmail.com>
2023-02-15 00:35:43 -0500

    initMapButton modified to pass contactID

--- app/src/main/java/com/example/mycontactlist/MainActivity.java
+++ app/src/main/java/com/example/mycontactlist/MainActivity.java
@@ -91,7 +91,16 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ContactMapActivity.class);
+                Intent intent = new Intent(MainActivity.this,
+                        ContactMapActivity.class);
+
+                if (currentContact.getContactID() == -1) {
+                    Toast.makeText(getBaseContext(), "Contact must be saved before it can be " +
+                            "mapped", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    intent.putExtra("contactid", currentContact.getContactID());
+                }
 
                 // Alert the operating system to not make multiple copies of the same activity
                 intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
